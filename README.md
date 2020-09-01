61c7cca2ff712134be09f460053078590d81252e
Joseph Hassell <joseph@hassell.dev>
2020-09-01 13:49:52 -0500

    Flutter Project INIT

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # shlink_app
+
 A flutter app that connects to your shlink instance.
---
+++ android/app/src/main/kotlin/com/example/shlink_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shlink_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
